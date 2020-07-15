575d0a14527e65058be49e12a4d1e70224c37a96
arnold-dev <arnold.fertin@univ-grenoble-alpes.fr>
2020-07-15 14:01:24 +0200

    Unet tested

--- UNet.jl
+++ UNet.jl
@@ -2,9 +2,11 @@
 upsample : ConvTranspose((2,2), ifilters=>ifilters/2, relu, stride=(2,2))
  ou upsample(x, ratio) puis Conv((2,2), ifilters=>ifilters/2) problem padding
  Conv((1,1))
-
+=#
 
 # https://github.com/FluxML/NNlib.jl/pull/95
+
+# Pb avec GPU ou Zygote???
 function upsample(x, ratio) #upsamle(img, (2,2,1,1)) x -> upsample(x, (2,2,1,1))
   y = similar(x, (size(x) .* ratio)...)
   for i in Iterators.product(Base.OneTo.(ratio)...)
@@ -13,43 +15,59 @@ function upsample(x, ratio) #upsamle(img, (2,2,1,1)) x -> upsample(x, (2,2,1,1))
   end
   return y
 end
-=#
+
+upsample(x) = upsample(x, (2,2,1,1))
 
 using Flux
-using Flux: @functor
+using Flux: @functor #, convfilter, Zeros
+using Distributions: Normal
 
-# ajout de batchnorm
-function conv3x3(filters; downsample)
+include("convwb.jl")
+
+#init_ones(shape...) = ones(Float32, shape...)
+
+#ifunc = glorot_uniform
+
+# Params : inchannnels, nclasses, activation, init, batchnorm
+
+function convbn(kernel, filters, activation, batchnorm, init, pad=(1,1))
   ifilters, ofilters = filters
   layers = []
-  if downsample
-    push!(layers, MaxPool((2,2)))
+  if batchnorm
+    push!(layers, ConvWB(kernel, filters, pad=pad, init=init))
+    push!(layers, BatchNorm(ofilters, activation))
+  else
+    push!(layers, Conv(kernel, filters, activation, pad=pad, init=init))
   end
-  push!(layers, Conv((3,3), ifilters=>ofilters, relu, pad=(1,1)))
-  push!(layers, Conv((3,3), ofilters=>ofilters, relu, pad=(1,1)))
   return layers
 end
 
-conv3x3(filters) = conv3x3(filters, downsample=false)
+function uconv(filters, activation, batchnorm, init)
+  ifilters, ofilters = filters
+  layers = []
+  push!(layers, convbn((3,3), ifilters=>ofilters, activation, batchnorm, init)...)
+  push!(layers, convbn((3,3), ofilters=>ofilters, activation, batchnorm, init)...)
+  return layers
+end
 
-struct DecoderBlock
-  upsample
+struct UBlock
+  up
   conv
 end
 
-@functor DecoderBlock
+@functor UBlock
 
-function DecoderBlock(filters)
+function UBlock(filters, activation, batchnorm, init)
   ifilters, ofilters = filters
   mfilters = Int(ifilters / 2)
-  upsample = ConvTranspose((2,2), ifilters=>mfilters, stride=(2,2))
-  conv = Chain(conv3x3(filters)...)
-  return DecoderBlock(upsample, conv)
+  up = ConvTranspose((2,2), ifilters=>mfilters, activation, stride=(2,2), init=init)
+  conv = Chain(uconv(filters, activation, batchnorm, init)...)
+  return UBlock(up, conv)
 end
 
-(d::DecoderBlock)(x, bridge) = d.conv(cat(d.upsample(x), bridge, dims=3))
+(d::UBlock)(x, bridge) = d.conv(cat(d.up(x), bridge, dims=3))
 
-struct Unet
+struct UNet
   enc1
   enc2
   enc3
@@ -62,31 +80,31 @@ struct Unet
   output
 end
 
-@functor Unet
+@functor UNet
 
-function Unet(;inchannels=3) # ajout du nombre de classes
-  enc1 = Chain(conv3x3(inchannels=>64)...)
-  enc2 = Chain(conv3x3(64=>128, downsample=true)...)
-  enc3 = Chain(conv3x3(128=>256, downsample=true)...)
-  enc4 = Chain(conv3x3(256=>512, downsample=true)...)
-  enc5 = Chain(conv3x3(512=>1024, downsample=true)...)
-  dec1 = DecoderBlock(1024=>512)
-  dec2 = DecoderBlock(512=>256)
-  dec3 = DecoderBlock(256=>128)
-  dec4 = DecoderBlock(128=>64)
-  output = Conv((1,1), 64=>1, sigmoid)
-  return Unet(enc1, enc2, enc3, enc4, enc5, dec1, dec2, dec3, dec4, output)
+function UNet(;inchannels, activation=relu, batchnorm=false, init=glorot_uniform) # ajout du nombre de classes
+  enc1 = Chain(uconv(inchannels=>64, activation, batchnorm, init)...)
+  enc2 = Chain(MaxPool((2,2)), uconv(64=>128, activation, batchnorm, init)...)
+  enc3 = Chain(MaxPool((2,2)), uconv(128=>256, activation, batchnorm, init)...)
+  enc4 = Chain(MaxPool((2,2)), uconv(256=>512, activation, batchnorm, init)...)
+  enc5 = Chain(MaxPool((2,2)), uconv(512=>1024, activation, batchnorm, init)...)
+  dec1 = UBlock(1024=>512, activation, batchnorm, init)
+  dec2 = UBlock(512=>256, activation, batchnorm, init)
+  dec3 = UBlock(256=>128, activation, batchnorm, init)
+  dec4 = UBlock(128=>64, activation, batchnorm, init)
+  output = Chain(convbn((1,1), 64=>1, sigmoid, batchnorm, init, (0,0))...)
+  return UNet(enc1, enc2, enc3, enc4, enc5, dec1, dec2, dec3, dec4, output)
 end
 
-function (u::Unet)(x)
+function (u::UNet)(x)
   b1 = u.enc1(x)
   b2 = u.enc2(b1)
   b3 = u.enc3(b2)
   b4 = u.enc4(b3)
   b5 = u.enc5(b4)
-  d = u.dec1(b5, b4)
-  d = u.dec2(d, b3)
-  d = u.dec3(d, b2)
-  d = u.dec4(d, b1)
-  return u.output(d)
+  d1 = u.dec1(b5, b4)
+  d2 = u.dec2(d1, b3)
+  d3 = u.dec3(d2, b2)
+  d4 = u.dec4(d3, b1)
+  return u.output(d4)
 end
--- test-unet.jl
+++ test-unet.jl
@@ -1,12 +1,142 @@
 include("UNet.jl")
+include("metrics.jl")
 
-p = "/media/afertin/Stock1/CNN_exp/datasets/fish_image/fish_01/"
-imgs = joinpath.(p, readdir(p));
+using ImageDraw
+using FileIO
+using Images
+using Augmentor
+using Statistics
+using Flux: binarycrossentropy, params, Params #dice_coeff_loss, bce_loss, , logitbce_loss
+using Base.Iterators: partition
+using Random
+using CUDAapi
+if has_cuda()
+  @info "CUDA is on"
+  import CuArrays
+  CuArrays.allowscalar(false)
+else
+  @info "CUDA is off"
+end
 
-p = "/media/afertin/Stock1/CNN_exp/datasets/fish_image/mask_01/"
-masks = joinpath.(p, readdir(p));
+function normalize(img)
+  img = permutedims(channelview(img), (3, 2, 1))
+  return Float32.(img) # .- 0.5) .* 200
+end
 
-model = Unet(inchannels=3) |> gpu
-opt = Momentum()
+# vérifier le resize sur ground truth toujours binaire
+function loadimages(idx, imdirs, mkdirs)
+  x = normalize(augment(load(imdirs[idx]), Resize((128,128))))
+  y = round.(Float32.(augment(load(mkdirs[idx]), Resize((128,128)))))
+  y = permutedims(y, (2, 1))[:,:,:] # verif manque permutedim
+  return x, y
+end
 
-Flux.train!(loss, Flux.params(u), rep, opt);
+p = "/media/afertin/Stock1/CNN_exp/datasets/fish_image"
+f = readdir(joinpath(p, "fish_01"));
+m = replace.(f, "fish"=>"mask")
+imgs = joinpath.(p, "fish_01", f);
+masks = joinpath.(p, "mask_01", m);
+
+function makeminibatch(idx, imgs, masks)
+  pairs = [loadimages(i, imgs, masks) for i in idx]
+  X = Flux.batch([pairs[i][1] for i in eachindex(pairs)])
+  Y = Flux.batch([pairs[i][2] for i in eachindex(pairs)])
+  return X, Y #!!!! Vérif dims de Y
+end
+
+function makeminibatchfake(idx, datas)
+  pairs = datas[idx]
+  X = Flux.batch([normalize(pairs[i][1]) for i in eachindex(pairs)]) #      [normalize(pairs[i][1]) for i in eachindex(pairs)])
+  Y = Flux.batch([Float32.(permutedims(pairs[i][2], (2, 1)))[:,:,:] for i in eachindex(pairs)])
+  return X, Y
+end
+
+function randomcircle()
+  img = RGB.(rand(128, 128) ./ 2, rand(128, 128) ./ 2, rand(128, 128) ./ 2)
+  o = round.((rand(2) .- 0.5) * 30) .+ 64
+  r = round.((rand(1) .- 0.5) * 10)[1] + 20
+  draw!(img, CirclePointRadius(Point(o...), r), RGB(1.0,1.0,1.0))
+  mask = Gray.(zeros(128, 128))
+  draw!(mask, CirclePointRadius(Point(o...), r), Gray(1.0))
+  return img, mask
+end
+
+ model = UNet(inchannels=3, batchnorm=true) |> gpu
+#opt = ADAM(1e-10)
+#opt = ADAM(1e-10)
+#opt = Descent(1e-3)
+#opt = Flux.Optimiser(ExpDecay(0.1, 0.1, 1000, 1e-4), Descent())
+opt = Descent()
+
+#bce(ŷ, y) = mean(binarycrossentropy.(ŷ, y))
+#loss(x, y) = dice_coeff_loss(model(x), y) #round. fout la merde
+#loss(x, y) = bce_loss(model(x), y)
+#loss(x, y) = logitbce_loss(model(x), y)
+
+#=
+function bce(ŷ, y; ϵ=gpu(fill(eps(first(ŷ)), size(ŷ)...)))
+  l1 = -y.*log.(ŷ .+ ϵ)
+  l2 = (1 .- y).*log.(1 .- ŷ .+ ϵ)
+  l1 .- l2
+end
+
+function loss(x, y)
+  op = clamp.(model(x), 0.001f0, 1.f0)
+  mean(bce(op, y))
+end
+=#
+#x = normalize(augment(load(imgs[1]), Resize((128,128)))) |> gpu
+#Flux.train!(loss, Flux.params(model), gpu.([(im, gt)]), opt)
+
+n = length(imgs)
+#datas = [randomcircle() for _ in 1:n]
+
+loss(x, y) = mean(Flux.binarycrossentropy.(model(x), y))
+loss(x, y) = Flux.dice_coeff_loss(model(x), y)
+
+xxx = []
+
+trainmet = Metrics((ŷ, y) -> 1 - Flux.dice_coeff_loss(ŷ, y), (ŷ, y) -> mean(Flux.binarycrossentropy.(ŷ, y)))
+for e in 1:20
+  for idx in partition(shuffle(1:n), 32) #
+    trainmode!(model)
+    batch = makeminibatch(idx, imgs, masks) |> gpu #makeminibatchfake(idx, datas) |> gpu # #img, gt = loadimages(i, imgs, masks) |> gpu
+    ps = Params(params(model))
+    gs = gradient(ps) do
+      training_loss = loss(batch...)
+      #println(training_loss)
+      return training_loss
+    end
+    Flux.Optimise.update!(opt, ps, gs)
+    update!(trainmet, model, batch...)
+  end
+  update!(trainmet)
+  @show trainmet
+end
+
+#=
+function my_custom_train!(loss, ps, data, opt)
+  ps = Flux.Params(ps)
+  for d in data
+    gs = gradient(ps) do
+      training_loss = loss(d...)
+      # Insert what ever code you want here that needs Training loss, e.g. logging
+      return training_loss
+    end
+    # insert what ever code you want here that needs gradient
+    # E.g. logging with TensorBoardLogger.jl as histogram so you can see if it is becoming huge
+    update!(opt, ps, gs)
+    # Here you might like to check validation set accuracy, and break out to do early stopping
+  end
+end
+
+d = gpu.([(im, gt)])[1]
+ps = Flux.params(model)
+gs = gradient(ps) do
+  training_loss = loss(d...)
+  # Insert what ever code you want here that needs Training loss, e.g. logging
+  return training_loss
+end
+
+
+=#
